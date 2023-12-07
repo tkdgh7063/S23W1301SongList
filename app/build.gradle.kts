c0e1a47c8ae08e680fa3f59b5969788bf453ef96
tkdgh7063 <hoo412298@gmail.com>
2023-12-08 06:17:59 +0900

    💄 AsyncImage 및 Card 사용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,10 @@ android {
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 
+    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
--- app/src/main/java/kr/ac/kumoh/ce/s20180630/s23w1301songlist/Song.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180630/s23w1301songlist/Song.kt
@@ -1,11 +1,9 @@
 package kr.ac.kumoh.ce.s20180630.s23w1301songlist
 
-import android.media.Rating
-
 data class Song(
     val id: Int,
     val title: String,
     val singer: String,
     val rating: Int,
-    val lyrics: String
+    val lyrics: String?
 )
